8ec19c53437d0eb79ff9d2be253c0f4e9456a66f
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-04-14 22:57:12 +0300

    Полный REBUILT - решил через класс Chat

--- HW9-1/main.cpp
+++ HW9-1/main.cpp
@@ -1,121 +1,169 @@
-//
-//  main.cpp
-//  HW9-1
-//
-//  Created by Alyaev Roman on 05.04.2021.
-//
+//REBUILT OF HW9-1
+//----------------------------------
+//Created by Alyaev Roman B04-906
+//on 14.04.2021
 
-#include <filesystem>
-#include <iostream>
-#include <thread>
-#include <string>
-#include <vector>
-#include <mutex>
-#include <condition_variable>
+#include <algorithm>
 #include <atomic>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <utility>
 
-#include <boost/interprocess/managed_shared_memory.hpp>
-#include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/containers/vector.hpp>
-#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/sync/interprocess_condition.hpp>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
 
-void cin_thread()
+using namespace boost::interprocess;
+
+class Chat
 {
-    static std::atomic<int> counter_of_examples = 0;
-    
-    using namespace boost::interprocess;
- 
-    typedef allocator < char, managed_shared_memory::segment_manager > CharAllocator;
-    typedef basic_string < char, std::char_traits<char>, CharAllocator > MyShmString;
-    typedef allocator < MyShmString, managed_shared_memory::segment_manager > StringAllocator;
-    typedef vector < MyShmString, StringAllocator > MyShmStringVector;
-    
-    const std::string shared_memory_name = "managed_shared_memory";
-    shared_memory_object::remove(shared_memory_name.c_str());
-    managed_shared_memory shared_memory(open_or_create, shared_memory_name.c_str(), 10000);
-    
-    //Create allocators
-    CharAllocator     charallocator  (shared_memory.get_segment_manager());
-    StringAllocator   stringallocator(shared_memory.get_segment_manager());
-    
-    MyShmStringVector *vector = shared_memory.construct < MyShmStringVector > ("vector")(stringallocator);
-    
-    auto mutex = shared_memory.construct < interprocess_mutex > ("mutex")();
-    auto condition_var = shared_memory.construct < interprocess_condition > ("condition_var")();
-    
-    MyShmString message(charallocator);
-    message = "EXIT_SUCCES";
-    
-    counter_of_examples++;
-    
-    do
+private:
+
+    using shared_memory_t = managed_shared_memory;
+    using manager_t = managed_shared_memory::segment_manager;
+    using string_allocator_t = allocator < char, manager_t >;
+    using string_t = basic_string < char, std::char_traits<char>, string_allocator_t >;
+    using vector_allocator_t = allocator < string_t, manager_t >;
+    using vector_t = vector < string_t, vector_allocator_t >;
+    using mutex_t = interprocess_mutex;
+    using condition_t = interprocess_condition;
+    using counter_t = std::atomic < std::size_t > ;
+
+public:
+
+    explicit Chat(const std::string & user_name) : m_user_name(user_name), m_exit_flag(false),
+        m_shared_memory(open_or_create, shared_memory_name.c_str(), 10000 * sizeof(char))
     {
+        m_vector    = m_shared_memory.construct < vector_t > ("vector_t")(m_shared_memory.get_segment_manager());
+        m_mutex     = m_shared_memory.construct < mutex_t > ("m_mutex")();
+        m_condition = m_shared_memory.construct < condition_t > ("m_condition")();
+        m_users     = 0;
+        m_messages  = 0;
+
+        m_local_messages = 0;
+
+        (*m_users)++;
+    }
+
+    ~Chat() noexcept = default;
+
+public:
+
+    void run()
+    {
+        auto reader = std::thread(&Chat::read, this);
+
+        write();
+        
+        m_exit_flag = true;
+        
+        m_condition->notify_all();
+
+        reader.join();
+
+        send_message(m_user_name + " left the chat");
+
+        if (!(--(*m_users)))
+        {
+            shared_memory_object::remove(shared_memory_name.c_str());
+        }
+    }
+
+private:
+
+    void read()
+    {
+        show_history();
+
+        send_message(m_user_name + " joined the chat");
+
+        while (true)
+        {
+            std::unique_lock < mutex_t > lock(*m_mutex);
+
+            m_condition->wait(lock, [this]() { return !this->m_vector->empty(); });
+
+            if (m_exit_flag)
+            {
+                break;
+            }
+
+            std::cout << m_vector->back() << std::endl;
+
+            m_vector->pop_back();
+        }
+    }
+
+    void show_history()
+    {
+        std::scoped_lock< mutex_t > lock(*m_mutex);
+
+        for (const auto & message : *m_vector)
+        {
+            std::cout << message << std::endl;
+        }
+    }
+
+    void send_message(const std::string & message)
+    {
+        std::scoped_lock < mutex_t > lock(*m_mutex);
+
+        m_vector->push_back(string_t(message.c_str(), m_shared_memory.get_segment_manager()));
+
+        m_condition->notify_all();
+
+        (*m_messages)++;
+
+        m_local_messages++;
+    }
+
+    void write()
+    {
+        std::string message;
+        
         std::cin >> message;
         
-        std::lock_guard < interprocess_mutex > lock(*mutex);
-        
-        vector->push_back(message);
-        
-        condition_var->notify_all();
+        send_message(message);
     }
-    while(message != "EXIT_SUCCESS");
-    
-    counter_of_examples--;
-    
-    if (counter_of_examples == 0)
-    {
-        shared_memory_object::remove(shared_memory_name.c_str());
-    }
-}
 
-void cout_thread()
-{
-    using namespace boost::interprocess;
- 
-    typedef allocator < char, managed_shared_memory::segment_manager > CharAllocator;
-    typedef basic_string < char, std::char_traits<char>, CharAllocator > MyShmString;
-    typedef allocator < MyShmString, managed_shared_memory::segment_manager > StringAllocator;
-    typedef vector < MyShmString, StringAllocator > MyShmStringVector;
-    
-    const std::string shared_memory_name = "managed_shared_memory";
-    managed_shared_memory shared_memory(open_only, shared_memory_name.c_str());
-    
-    //Create allocators
-    CharAllocator     charallocator  (shared_memory.get_segment_manager());
-    StringAllocator   stringallocator(shared_memory.get_segment_manager());
-    
-    MyShmStringVector *vector = shared_memory.find < MyShmStringVector > ("vector").first;
-    
-    auto mutex = shared_memory.find < interprocess_mutex > ("mutex").first;
-    auto condition_var = shared_memory.find < interprocess_condition > ("condition_var").first;
-    
-    MyShmString message(charallocator);
-    message = "EXIT_SUCCES";
-    
-    do
-    {
-        std::unique_lock lock(*mutex);
-        
-        condition_var->wait(lock, [vector](){return !vector->empty();});
-        
-        message = vector->back();
-        
-        std::cout << message << std::endl;
-        
-        vector->pop_back();
-    }
-    while(message != "EXIT_SUCCESS");
-}
+private:
 
-int main(int argc, const char * argv[])
+    static inline const std::string shared_memory_name = "shared_memory";
+
+private:
+
+    std::string m_user_name;
+
+    std::atomic < bool > m_exit_flag;
+
+    shared_memory_t m_shared_memory;
+
+    vector_t    * m_vector;
+    mutex_t     * m_mutex;
+    condition_t * m_condition;
+    counter_t   * m_users;
+    counter_t   * m_messages;
+
+    std::size_t m_local_messages;
+};
+
+int main(int argc, char ** argv)
 {
-    std::thread input_thread(cin_thread);
-    std::thread output_thread(cout_thread);
-    
-    input_thread.join();
-    output_thread.join();
-    
-    return 0;
+    std::string user_name;
+
+    std::cout << "Enter your name: ";
+
+    std::getline(std::cin, user_name);
+
+    Chat(user_name).run();
+
+    system("pause");
+
+    return EXIT_SUCCESS;
 }
